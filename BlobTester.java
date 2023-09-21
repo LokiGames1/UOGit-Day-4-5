3758ffe341e4e989e70b4ff4274554a8a7caaa02
Asher Engelberg <ashercengelberg@icloud.com>
2023-09-21 09:50:48 -0700

    huh
    unsure

--- BlobTester.java
+++ BlobTester.java
@@ -5,7 +5,7 @@ public class BlobTester {
     public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
 
         Index indy = new Index();
-        indy.add("test.txt");
         indy.delete("test.txt");
+
     }
 }
